780a9aa5ef12112b0702042d1780efa0b54e44a4
Mehdi Baneshi <Baneshi@gmail.com>
2024-12-16 23:20:02 +0330

    change link and text on home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to Resume Builder</h1>
+<p>Visit <a href="https://github.com/mbaneshi">my github profile</a> to see my latest projects</p>
